f7e3687b08981347b43c6d0c3b90eff51578f024
Sahan Thenuwara <t.a.s.sanjith@gmail.com>
2023-11-14 14:38:56 +0530

    added user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,78 @@
+const User = require("../models/user");
+const { verifyTokenAndAuth, verifyTokenAndAdmin } = require("./verifyToken");
+
 const router = require("express").Router();
 
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new;
+    try {
+        const users = query
+            ? await User.find().sort({ _id: -1 }).limit(5)
+            : await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
-router.get("/", (req, res) => {
-    res.send("This is user route");
-})
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+    try {
+        const data = await User.aggregate([
+            { $match: { createdAt: { $gte: lastYear } } },
+            { $project: { month: { $month: "$createdAt" } } },
+            { $group: { _id: "$month", total: { $sum: 1 } } },
+        ]);
+        console.log(data);
 
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("This is user post route");
-})
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
-module.exports = router
+router.get("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.put("/:id", verifyTokenAndAuth, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SECRET
+        ).toString();
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+
+        const { password, ...others } = updatedUser._doc;
+
+        res.status(200).json(others);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.delete("/:id", verifyTokenAndAuth, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+module.exports = router;
